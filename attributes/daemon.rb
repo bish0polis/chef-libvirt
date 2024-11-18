01699426b982e5d9f05194154a0868a765da8347
bishop <bishopolis@gmail.com>
2024-11-18 11:43:18 -0800

    feat: move '=' to '||=' in the hopes of preserving any pre-gamed values.

--- attributes/daemon.rb
+++ attributes/daemon.rb
@@ -17,59 +17,59 @@
 # limitations under the License.
 #
 
-default["libvirt"]["daemon"]["service_name"] = value_for_platform_family(
+default["libvirt"]["daemon"]["service_name"] ||= value_for_platform_family(
   "debian" => "libvirt-bin",
   "ubuntu" => "libvirt-bin",
   "suse" => "libvirtd",
   "rhel" => "libvirtd"
 )
-default["libvirt"]["daemon"]["sysconfig_file"] = value_for_platform_family(
+default["libvirt"]["daemon"]["sysconfig_file"] ||= value_for_platform_family(
   "debian" => "/etc/default/libvirt-bin",
   "ubuntu" => "/etc/default/libvirt-bin",
   "suse" => "/etc/sysconfig/libvirtd",
   "rhel" => "/etc/sysconfig/libvirtd"
 )
 
-default["libvirt"]["daemon"]["group"] = value_for_platform_family(
+default["libvirt"]["daemon"]["group"] ||= value_for_platform_family(
   "debian" => "libvirtd",
   "ubuntu" => "libvirtd",
   "suse" => "libvirtd",
   "rhel" => "root"
 )
 
-default["libvirt"]["daemon"]["emulator"] = value_for_platform_family(
+default["libvirt"]["daemon"]["emulator"] ||= value_for_platform_family(
   "debian" => "/usr/bin/qemu-kvm",
   "ubuntu" => "/usr/bin/qemu-kvm",
   "suse" => "/usr/bin/qemu-kvm",
   "rhel" => "/usr/libexec/qemu-kvm"
 )
 
-default["libvirt"]["daemon"]["config_file"] = "/etc/libvirt/libvirtd.conf"
-default["libvirt"]["daemon"]["libvirt_file"] = "/etc/libvirt/libvirt.conf"
-default["libvirt"]["daemon"]["uri_aliases"] = []
-default["libvirt"]["daemon"]["uri_default"] = "qemu:///system"
+default["libvirt"]["daemon"]["config_file"] ||= "/etc/libvirt/libvirtd.conf"
+default["libvirt"]["daemon"]["libvirt_file"] ||= "/etc/libvirt/libvirt.conf"
+default["libvirt"]["daemon"]["uri_aliases"] ||= []
+default["libvirt"]["daemon"]["uri_default"] ||= "qemu:///system"
 
-default["libvirt"]["daemon"]["lxc_file"] = "/etc/libvirt/lxc.conf"
-default["libvirt"]["daemon"]["lxc"]["log_with_libvirtd"] = 1
-default["libvirt"]["daemon"]["lxc"]["security_driver"] = "none"
-default["libvirt"]["daemon"]["lxc"]["security_default_confined"] = 0
-default["libvirt"]["daemon"]["lxc"]["security_require_confined"] = 0
+default["libvirt"]["daemon"]["lxc_file"] ||= "/etc/libvirt/lxc.conf"
+default["libvirt"]["daemon"]["lxc"]["log_with_libvirtd"] ||= 1
+default["libvirt"]["daemon"]["lxc"]["security_driver"] ||= "none"
+default["libvirt"]["daemon"]["lxc"]["security_default_confined"] ||= 0
+default["libvirt"]["daemon"]["lxc"]["security_require_confined"] ||= 0
 
-default["libvirt"]["daemon"]["qemu_file"] = "/etc/libvirt/qemu.conf"
-default["libvirt"]["daemon"]["qemu"]["security_driver"] = "none"
-default["libvirt"]["daemon"]["qemu"]["security_default_confined"] = 0
-default["libvirt"]["daemon"]["qemu"]["security_require_confined"] = 0
+default["libvirt"]["daemon"]["qemu_file"] ||= "/etc/libvirt/qemu.conf"
+default["libvirt"]["daemon"]["qemu"]["security_driver"] ||= "none"
+default["libvirt"]["daemon"]["qemu"]["security_default_confined"] ||= 0
+default["libvirt"]["daemon"]["qemu"]["security_require_confined"] ||= 0
 
-default["libvirt"]["daemon"]["qemulockd_file"] = "/etc/libvirt/qemu-lockd.conf"
-default["libvirt"]["daemon"]["qemulockd"]["auto_disk_leases"] = 0
-default["libvirt"]["daemon"]["qemulockd"]["require_lease_for_disks"] = 1
-default["libvirt"]["daemon"]["qemulockd"]["file_lockspace_dir"] = "/var/lib/libvirt/lockd/files"
-default["libvirt"]["daemon"]["qemulockd"]["lvm_lockspace_dir"] = "/var/lib/libvirt/lockd/lvmvolumes"
-default["libvirt"]["daemon"]["qemulockd"]["scsi_lockspace_dir"] = "/var/lib/libvirt/lockd/scsivolumes"
+default["libvirt"]["daemon"]["qemulockd_file"] ||= "/etc/libvirt/qemu-lockd.conf"
+default["libvirt"]["daemon"]["qemulockd"]["auto_disk_leases"] ||= 0
+default["libvirt"]["daemon"]["qemulockd"]["require_lease_for_disks"] ||= 1
+default["libvirt"]["daemon"]["qemulockd"]["file_lockspace_dir"] ||= "/var/lib/libvirt/lockd/files"
+default["libvirt"]["daemon"]["qemulockd"]["lvm_lockspace_dir"] ||= "/var/lib/libvirt/lockd/lvmvolumes"
+default["libvirt"]["daemon"]["qemulockd"]["scsi_lockspace_dir"] ||= "/var/lib/libvirt/lockd/scsivolumes"
 
-default["libvirt"]["daemon"]["virtlockd_file"] = "/etc/libvirt/virtlockd.conf"
-default["libvirt"]["daemon"]["virtlockd"]["log_filters"] = "3:remote 4:event"
-default["libvirt"]["daemon"]["virtlockd"]["log_outputs"] = "3:syslog:virtlockd"
-default["libvirt"]["daemon"]["virtlockd"]["log_level"] = 3
-default["libvirt"]["daemon"]["virtlockd"]["log_buffer_size"] = 64
-default["libvirt"]["daemon"]["virtlockd"]["max_clients"] = 1024
+default["libvirt"]["daemon"]["virtlockd_file"] ||= "/etc/libvirt/virtlockd.conf"
+default["libvirt"]["daemon"]["virtlockd"]["log_filters"] ||= "3:remote 4:event"
+default["libvirt"]["daemon"]["virtlockd"]["log_outputs"] ||= "3:syslog:virtlockd"
+default["libvirt"]["daemon"]["virtlockd"]["log_level"] ||= 3
+default["libvirt"]["daemon"]["virtlockd"]["log_buffer_size"] ||= 64
+default["libvirt"]["daemon"]["virtlockd"]["max_clients"] ||= 1024
