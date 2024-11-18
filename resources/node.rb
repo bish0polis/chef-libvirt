7aee842b94de024a7d73a7ce44e0c799447ec8ed
bishop <bishopolis@gmail.com>
2024-11-18 14:07:26 -0800

    - jammed providers/node.rb into resources/node.rb

--- resources/node.rb
+++ resources/node.rb
@@ -17,14 +17,266 @@
 # limitations under the License.
 #
 
-actions :create, :define, :destroy, :start, :undefine, :autoboot, :noboot
+#actions :create, :define, :destroy, :start, :undefine, :autoboot, :noboot
+provides	:node
 
-attribute :name, :kind_of => String, :name_attribute => true
-attribute :uuid, :kind_of => String, :default => nil
-attribute :memory, :kind_of => Integer, :default => 1024
-attribute :cpus, :kind_of => Integer, :default => 1
-attribute :vnc, :kind_of => Integer, :default => 5900
-attribute :disks, :kind_of => Array, :default => []
-attribute :interfaces, :kind_of => Array, :default => []
+property :name, String, name_property: true
+property :uuid, String, default: nil
+property :memory, Integer, default: 1024
+property :cpus, Integer, default: 1
+property :vnc, Integer, default: 5900
+property :disks, Array, default: []
+property :interfaces, Array, default: []
 
-default_action :create
+action :create do
+  template create_xml_path do
+    owner "root"
+    group "root"
+    mode 0640
+
+    cookbook "libvirt"
+    source "node.xml.erb"
+
+    variables(
+      "name" => new_resource.name,
+      "uuid" => new_resource.uuid,
+      "memory" => new_resource.memory,
+      "cpus" => new_resource.cpus,
+      "vnc" => new_resource.vnc,
+      "disks" => extended_disks,
+      "interfaces" => extended_interfaces
+    )
+
+    notifies :run, "bash[virsh_node_create_#{new_resource.name}]", :immediately
+  end
+
+  bash "virsh_node_create_#{new_resource.name}" do
+    code <<-EOH
+      virsh create #{create_xml_path}
+    EOH
+
+    action :run
+    notifies :reload, "service[libvirt-daemon]"
+
+    not_if do
+      all_include? new_resource.name
+    end
+  end
+
+  new_resource.updated_by_last_action(true)
+end
+
+action :define do
+  template define_xml_path do
+    owner "root"
+    group "root"
+    mode 0640
+
+    cookbook "libvirt"
+    source "node.xml.erb"
+
+    variables(
+      "name" => new_resource.name,
+      "uuid" => new_resource.uuid,
+      "memory" => new_resource.memory,
+      "cpus" => new_resource.cpus,
+      "vnc" => new_resource.vnc,
+      "disks" => extended_disks,
+      "interfaces" => extended_interfaces
+    )
+
+    notifies :run, "bash[virsh_node_define_#{new_resource.name}]", :immediately
+  end
+
+  bash "virsh_node_define_#{new_resource.name}" do
+    code <<-EOH
+      virsh define #{define_xml_path}
+    EOH
+
+    action :run
+    notifies :reload, "service[libvirt-daemon]"
+
+    not_if do
+      all_include? new_resource.name
+    end
+  end
+
+  new_resource.updated_by_last_action(true)
+end
+
+action :undefine do
+  if inactive_include? new_resource.name
+    bash "virsh_node_undefine_#{new_resource.name}" do
+      code <<-EOH
+        virsh undefine #{new_resource.name}
+      EOH
+
+      action :run
+    end
+
+    new_resource.updated_by_last_action(true)
+  else
+    Chef::Log.debug "Can't undefine, node #{new_resource.name} is not in inactive list"
+  end
+end
+
+action :start do
+  if inactive_include? new_resource.name
+    bash "virsh_node_start_#{new_resource.name}" do
+      code <<-EOH
+        virsh start #{new_resource.name}
+      EOH
+
+      action :run
+    end
+
+    new_resource.updated_by_last_action(true)
+  else
+    Chef::Log.debug "Can't start, node #{new_resource.name} is not in inactive list"
+  end
+end
+
+action :destroy do
+  if active_include? new_resource.name
+    bash "virsh_node_destroy_#{new_resource.name}" do
+      code <<-EOH
+        virsh destroy #{new_resource.name}
+      EOH
+
+      action :run
+    end
+
+    new_resource.updated_by_last_action(true)
+  else
+    Chef::Log.debug "Can't destroy, node #{new_resource.name} is not in active list"
+  end
+end
+
+action :autoboot do
+  if noboot_include? new_resource.name
+    bash "virsh_node_autoboot_#{new_resource.name}" do
+      code <<-EOH
+        virsh autostart #{new_resource.name}
+      EOH
+
+      action :run
+    end
+
+    new_resource.updated_by_last_action(true)
+  else
+    Chef::Log.debug "Can't autoboot, node #{new_resource.name} is not in noboot list"
+  end
+end
+
+action :noboot do
+  if autoboot_include? new_resource.name
+    bash "virsh_node_noboot_#{new_resource.name}" do
+      code <<-EOH
+        virsh autostart #{new_resource.name} --disable
+      EOH
+
+      action :run
+    end
+
+    new_resource.updated_by_last_action(true)
+  else
+    Chef::Log.debug "Can't noboot, node #{new_resource.name} is not in autoboot list"
+  end
+end
+
+#protected
+action_class do
+
+def create_xml_path
+  @create_xml_path ||= begin
+    ::File.join(
+      Chef::Config[:file_cache_path],
+      "virsh_node_create_#{new_resource.name}.xml"
+    )
+  end
+end
+
+def define_xml_path
+  @create_xml_path ||= begin
+    ::File.join(
+      Chef::Config[:file_cache_path],
+      "virsh_node_define_#{new_resource.name}.xml"
+    )
+  end
+end
+
+def extended_disks
+  disks = new_resource.disks.dup
+
+  disks.map do |disk|
+    if disk["download"]
+      remote_file disk["source"] do
+        source disk["download"]
+        action :nothing
+      end.run_action(:create_if_missing)
+    end
+
+    case disk["type"]
+    when "block"
+      disk["driver_type"] = "raw"
+      disk["source_type"] = "dev"
+    when "file"
+      execute "libvirt_image_create_#{disk["source"]}" do
+        command "qemu-img create -f qcow2 #{disk["source"]} #{disk["size"] || "30G"}"
+        action :nothing
+
+        user "root"
+        group "root"
+
+        not_if do
+          ::File.exists? disk["source"]
+        end
+      end.run_action(:run)
+
+      disk["driver_type"] = "qcow2"
+      disk["source_type"] = "file"
+    end
+
+    disk
+  end
+end
+
+def extended_interfaces
+  interfaces = new_resource.interfaces.dup
+
+  interfaces.map do |interface|
+    case interface["type"]
+    when "bridge"
+      interface["source_type"] = "bridge"
+    when "network"
+      interface["source_type"] = "network"
+    end
+
+    interface
+  end
+end
+
+def all_include?(name)
+  listing_include? "--all", name
+end
+
+def autoboot_include?(name)
+  listing_include? "--autostart", name
+end
+
+def noboot_include?(name)
+  listing_include? "--no-autostart", name
+end
+
+def inactive_include?(name)
+  listing_include? "--inactive", name
+end
+
+def active_include?(name)
+  listing_include? "", name
+end
+
+def listing_include?(flag, name)
+  `virsh list #{flag} | grep #{name} | wc -l`.strip != "0"
+end
+end	#action_class
