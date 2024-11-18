d0ee6c65b27e961389ddfdf88c9ce52a5ad3c348
bishop <bishopolis@gmail.com>
2024-11-18 11:45:25 -0800

    - chore: reformat the metadata and - maybe lose the 'network' dependency as it's rotted to nonexistence.

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,17 @@
-name "libvirt"
-maintainer "Thomas Boerger"
-maintainer_email "tboerger@tbpro.de"
-license "Apache 2.0"
-description "Installs/Configures libvirt"
-long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version "0.0.1"
-depends "network"
-recipe "libvirt", "Installs/Configures libvirt"
+version		"0.0.1"
 
-supports "suse", ">= 12.3"
-supports "debian", ">= 7.0"
-supports "ubuntu", ">= 12.04"
-supports "rhel", ">= 0.0"
+name		"libvirt"
+maintainer	"Thomas Boerger"
+maintainer_email "tboerger@tbpro.de"
+license		"Apache 2.0"
+description	"Installs/Configures libvirt"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+
+supports	"suse", ">= 12.3"
+supports	"debian", ">= 7.0"
+supports	"ubuntu", ">= 12.04"
+supports	"rhel", ">= 4.0"
+
+recipe		"libvirt", "Installs/Configures libvirt"
+
+#depends		"network"
